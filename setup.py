467276c0a3c4c259690cc23be5ec0378678a330b
Joel Cornett <joel.cornett@gmail.com>
2014-12-17 16:43:33 -0500

    initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(
+    name="pCVS",
+    version="0.1",
+    description="Python CVS wrapper",
+    author="Joel Cornett",
+    author_email="joel.cornett@gmail.com",
+    url="",
+    py_modules=["pcvs"]
+    )
